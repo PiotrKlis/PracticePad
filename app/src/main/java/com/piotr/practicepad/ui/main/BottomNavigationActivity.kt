474a17b65ac93be0efcdf5844800c9689a969dd3
Piotr Klis <piotr.klis@tigerspike.com>
2018-12-15 18:34:31 +0100

    initial commit

---
+++ app/src/main/java/com/piotr/practicepad/ui/main/BottomNavigationActivity.kt
@@ -0,0 +1,41 @@
+package com.piotr.practicepad.ui.main
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import com.piotr.practicepad.R
+import kotlinx.android.synthetic.main.activity_bottom_navigation.*
+
+class BottomNavigationActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, ExerciseFragment.newInstance())
+                    .commitNow()
+            }
+            R.id.navigation_dashboard -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, ExerciseListFragment.newInstance())
+                    .commitNow()
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        navigateToInitialScreen()
+    }
+
+    private fun navigateToInitialScreen() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, ExerciseFragment.newInstance())
+            .commitNow()
+    }
+
+
+}
---
+++ app/src/main/java/com/piotr/practicepad/ui/main/ExerciseListFragment.kt
@@ -0,0 +1,67 @@
+package com.piotr.practicepad.ui.main
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.piotr.practicepad.R
+import com.piotr.practicepad.ui.main.dummy.DummyContent
+import com.piotr.practicepad.ui.main.dummy.DummyContent.DummyItem
+
+/**
+ * A fragment representing a list of Items.
+ * Activities containing this fragment MUST implement the
+ * [ExerciseListFragment.OnListFragmentInteractionListener] interface.
+ */
+class ExerciseListFragment : Fragment() {
+    private var listener: OnListFragmentInteractionListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_exerciseset_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                adapter = MyExerciseSetRecyclerViewAdapter(DummyContent.ITEMS, listener)
+            }
+        }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnListFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson
+     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onListFragmentInteraction(item: DummyItem?)
+    }
+
+    companion object {
+        fun newInstance() = ExerciseListFragment()
+    }
+}
