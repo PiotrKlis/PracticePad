79f0da625ee0e708f8b9b472ed217e8b66c23065
Piotr Kliś <piotr.klis@tigerspike.com>
2019-02-12 10:06:02 +0100

    Timer works as expected

--- app/src/main/java/com/piotr/practicepad/ui/main/Exercise/ExerciseFragment.kt
+++ app/src/main/java/com/piotr/practicepad/ui/main/Exercise/ExerciseFragment.kt
@@ -3,7 +3,7 @@ package com.piotr.practicepad.ui.main.Exercise
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.os.Handler
+import android.os.CountDownTimer
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,21 +15,15 @@ import kotlinx.android.synthetic.main.fragment_excercise_set.*
 
 class ExerciseFragment : Fragment() {
 
-    var timerIsOn = false
-    var timerRunnable: Runnable = object : Runnable {
-
-        override fun run() {
-            overall_time.setText(String.format("%d:%02d", 30000, 200))
-            timerHandler.postDelayed(this, 500)
-        }
-    }
+    var isTimerOn = false
+    var timerRunnable: Runnable? = null
+    var startTimeValue: Int = 0
+    var currentTimeValue: Long = 0
+    var countDownTimer: CountDownTimer? = null
 
     companion object {
         fun newInstance() = ExerciseFragment()
     }
-
-    private lateinit var viewModel: ExerciseViewModel
-    val timerHandler = Handler()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_excercise_set, container, false)
@@ -37,7 +31,7 @@ class ExerciseFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ExerciseViewModel().javaClass)
+        val viewModel: ExerciseViewModel = ViewModelProviders.of(this).get(ExerciseViewModel().javaClass)
         viewModel.getExercises()?.observe(this, Observer<ExerciseSetData>
         { exercise -> renderElements(exercise?.exerciseDataList) })
     }
@@ -54,22 +48,34 @@ class ExerciseFragment : Fragment() {
 
     private fun initializePowerButton() {
         power.setOnClickListener {
-            if (timerIsOn) {
-                timerIsOn = false
+            if (isTimerOn) {
+                isTimerOn = false
                 stopTimer()
             } else {
-                timerIsOn = true
+                isTimerOn = true
                 startTimer()
             }
         }
     }
 
     private fun stopTimer() {
-        timerHandler.removeCallbacks(timerRunnable)
+        countDownTimer?.cancel()
+        isTimerOn = false
     }
 
     private fun startTimer() {
-        timerHandler.postDelayed(timerRunnable, 0)
+        countDownTimer = object : CountDownTimer(currentTimeValue, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                currentTimeValue = millisUntilFinished
+                updateCountDownText()
+            }
+
+            override fun onFinish() {
+                isTimerOn = false
+            }
+        }.start()
+
+        isTimerOn = true
     }
 
     private fun initializeImageOfCurrentExercise(exerciseList: List<ExerciseData>?) {
@@ -93,22 +99,22 @@ class ExerciseFragment : Fragment() {
     }
 
     private fun initializeOverallTimer(exerciseList: List<ExerciseData>?) {
-        var seconds = 0.0
+        var seconds: Long = 0
         if (exerciseList != null) {
             for (i in exerciseList) {
                 val value = i.time
                 seconds += value
             }
+            currentTimeValue = seconds
         }
-        val minutes = seconds / 60
-//        timerRunnable = object : Runnable {
-//
-//            override fun run() {
-//                overall_time.setText(String.format("%d:%02d", minutes, seconds))
-//                timerHandler.postDelayed(this, 500)
-//            }
-//        }
     }
 
+    private fun updateCountDownText() {
+        val minutes = (currentTimeValue / 1000).toInt() / 60
+        val seconds = (currentTimeValue / 1000).toInt() % 60
 
+        val timeLeftFormatted = String.format("%02d:%02d", minutes, seconds)
+
+        overall_time.text = timeLeftFormatted
+    }
 }
