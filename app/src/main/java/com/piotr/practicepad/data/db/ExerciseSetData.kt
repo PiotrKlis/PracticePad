e4939f84f58acd971cab21b2b6e41c8e55923695
PK <piotr.klis0@gmail.com>
2020-02-09 20:05:09 +0100

    deleted redundant exercise sets

--- app/src/main/java/com/piotr/practicepad/data/db/ExerciseSetData.kt
+++ app/src/main/java/com/piotr/practicepad/data/db/ExerciseSetData.kt
@@ -8,31 +8,7 @@ enum class ExerciseSetData(
     val exerciseDataList: List<ExerciseData>,
     val tempo: Int
 ) {
-    BEGINNER(0, "Beginner", getBeginnerSet(), 100),
-    INTERMEDIATE(1, "Intermediate", getAdvancedSet(), 80),
-    MASTER(2, "Master", getMasterSet(), 120),
-    EVERY_DAY(3, "Every Day Practice", everyDayPractice, 100)
-}
-
-private fun getBeginnerSet(): List<ExerciseData> {
-    return listOf(
-        ExerciseData(8000, SINGLE_STROKE_ROLL),
-        ExerciseData(7000, SINGLE_STROKE_FOUR),
-        ExerciseData(4000, SINGLE_STROKE_SEVEN))
-}
-
-private fun getAdvancedSet(): List<ExerciseData> {
-    return listOf(
-        ExerciseData(6000, MULTIPLE_BOUNCE_ROLL),
-        ExerciseData(3000, TRIPLETS),
-        ExerciseData(4000, DOUBLES))
-}
-
-private fun getMasterSet(): List<ExerciseData> {
-    return listOf(
-        ExerciseData(5000, MULTIPLE_BOUNCE_ROLL),
-        ExerciseData(4000, TRIPLE_STROKE_ROLL),
-        ExerciseData(4000, DOUBLE_STROKE_OPEN_ROLL))
+    EVERY_DAY(0, "Every Day Practice", everyDayPractice, 100)
 }
 
 private val everyDayPractice = listOf(
